8e5baa41cd3910a8717b9e14c7e06a7b28cf4dd8
Richard T. Widjaja <richard.tirta@akqa.com>
2013-04-17 17:06:20 -0700

    Clean up modular

--- main.js
+++ main.js
@@ -1,197 +1,122 @@
 // JavaScript Document
+var APP = (function () {
 
+	// constructor
+	var app = {};
 
-(function ($) { 
+	app.carousselInit = function(){
+		var imgCount = $("#slide-image>div").length,
+			loopTimer = null,
+			windowWidth = $(window).innerWidth(),
+			imageWidth = windowWidth,
+			imageIndex = 0,
+			currentImg = undefined,
+			currentCaption = undefined,
+			imageHeight = undefined;
 
-	
-	/*Global Variables*/
-	var isIpad = /ipad/i.test(navigator.userAgent.toLowerCase());
+		var initialSlide = function(n){
+			currentImg = $("#slide-image>div").eq(imageIndex);
+			currentCaption = $("#slide-caption>div").eq(imageIndex);
 
-	/*Carrousel Variable*/
-	var imageIndex = 0,
-		imgCount,
-		currentImg,						// Current image
-		currentCaption,
-		imageWidth = 1366,
-		loopTimer = null;
-
-
-	/*Carrousel Functions*/
-
-	function initialSlide(n) {
-
-		currentImg = $("#slide-image>div").eq(imageIndex);
-		currentCaption = $("#slide-caption>div").eq(imageIndex);
-
-		$("#slide-image>div").eq(imageIndex).siblings().css("left", imageWidth + "px");
-		$("#slide-caption>div").eq(imageIndex).siblings().css("left", imageWidth + "px");
-		
-		imageIndex = n;
-	}
-
-	function showSlide(n) {
-		var x = -imageWidth;						//variable if prev button clicked
-		if (n > imageIndex) {x = imageWidth; }		//variable if next button clicked
-
-		$(currentImg).stop().animate({left: -x + "px"}, 800, "swing");
-		currentImg = $("#slide-image>div").eq(n).css("left", x + "px").stop().animate({left: "0"}, 800, "swing");
-
-		$(currentCaption).stop().animate({left: -x + "px"}, 800, "swing");
-		currentCaption = $("#slide-caption>div").eq(n).css("left", x + "px").stop().animate({left: "0"}, 800, "swing");
-
-		$(".caption-wrapper").eq(n).hide().delay(500).fadeIn(800);
-
-		imageIndex = n;
-	}
-
-	function prevLoop(n) {							//Loop the last image as previous
-		var x = -imageWidth;
-
-		$(currentImg).stop().animate({left: -x + "px"}, 800, "swing");
-		currentImg = $("#slide-image>div").eq(n).css("left", x + "px").stop().animate({left: "0"}, 800, "swing");
-
-		$(currentCaption).stop().animate({left: -x + "px"}, 800, "swing");
-		currentCaption = $("#slide-caption>div").eq(n).css("left", x + "px").stop().animate({left: "0"}, 800, "swing");
-
-		$(".caption-wrapper").eq(n).hide().delay(500).fadeIn(800);
-
-		imageIndex = n;
-	}
-
-	function nextLoop(n) {							//Loop the first image as next			
-		var x = imageWidth;
-		
-		$(currentImg).stop().animate({left: -x + "px"}, 800, "swing");
-		currentImg = $("#slide-image>div").eq(n).css("left", x + "px").stop().animate({left: "0"}, 800, "swing");
-
-		$(currentCaption).stop().animate({left: -x + "px"}, 800, "swing");
-		currentCaption = $("#slide-caption>div").eq(n).css("left", x + "px").stop().animate({left: "0"}, 800, "swing");
-
-		$(".caption-wrapper").eq(n).hide().delay(500).fadeIn(800);
-
-		imageIndex = n;
-	}
-
-	function nextSlide() {
-		var n = imageIndex + 1;
-		if (n == imgCount) {
-			n = 0;
-			nextLoop(n);
-		} else {
-			showSlide(n);
+			$("#slide-image>div").eq(imageIndex).siblings().css("left", imageWidth + "px");
+			$("#slide-caption>div").eq(imageIndex).siblings().css("left", imageWidth + "px");
+			
+			imageIndex = n;
 		}
-	}
 
-	function prevSlide() {
-		var n = imageIndex - 1;
-		if (n < 0) {
-			n = imgCount - 1;
-			prevLoop(n);
-		} else {
-			showSlide(n);
+		var moveSlide = function(n,x){
+
+			$(currentImg).stop().animate({left: -x + "px"}, 800, "swing");
+			currentImg = $("#slide-image>div").eq(n).css("left", x + "px").stop().animate({left: "0"}, 800, "swing");
+
+			$(currentCaption).stop().animate({left: -x + "px"}, 800, "swing");
+			currentCaption = $("#slide-caption>div").eq(n).css("left", x + "px").stop().animate({left: "0"}, 800, "swing");
+
+			$(".caption-wrapper").eq(n).hide().delay(500).fadeIn(800);
+
+			imageIndex = n;
 		}
-	}
 
-	function autoSlide(){
-		if (!isIpad) {
+		var showSlide = function(n){
+			var x = -imageWidth;						//variable if prev button clicked
+			if (n > imageIndex) {x = imageWidth; }		//variable if next button clicked
+
+			moveSlide(n,x);
+			
+		}
+
+		var prevLoop = function(n){
+			var x = -imageWidth;
+
+			moveSlide(n,x);
+		}
+
+		var nextLoop = function(n){
+			var x = imageWidth;
+		
+			moveSlide(n,x);
+		}
+
+		var nextSlide = function(){
+			var n = imageIndex + 1;
+			if (n == imgCount) {
+				n = 0;
+				nextLoop(n);
+			} else {
+				showSlide(n);
+			}
+		}
+
+		var prevSlide = function(){
+			var n = imageIndex - 1;
+			if (n < 0) {
+				n = imgCount - 1;
+				prevLoop(n);
+			} else {
+				showSlide(n);
+			}
+		}
+
+		var autoSlide = function(){
 			clearInterval(loopTimer);
+
 			loopTimer = setInterval( function() {
 				nextSlide();
 			}, 10000 );
 		}
-	}
 
+		var mediaRespond = function(){
+			windowWidth = $(window).innerWidth();
+			imageWidth  = windowWidth;
+			imageHeight = (windowWidth*20)/33;
 
-	/*Media Responsive Function*/
+			$("#slide-show").css("width", windowWidth );
+			$("#slide-show").css("height", imageHeight );
 
-	function mediaRespond() {
-		var windowWidth = $(window).innerWidth();
-		var windowHeight = $(window).innerHeight();
-		var imageHeight;
-		var gdc = windowWidth - windowHeight;
-		var aspectRatio = (windowWidth/gdc)/(windowHeight/gdc);
-		var calculateWidth = false;
-
-		if (aspectRatio >= 1.25 && aspectRatio <= 1.6){
-			calculateWidth = true;
+			initialSlide(imageIndex);
 		}
 		
-		if(window.innerHeight > window.innerWidth || calculateWidth == true){
-			if (windowWidth >700 ) {
-				$("#wrapper").css("width", windowWidth);
-				$("#slide-show").css("width", windowWidth);	
-				$("#slide-image").css("width", windowWidth);
-				imageHeight = $("#slide-image>div>img").innerHeight();
-				imageWidth = $("#slide-image>div>img").innerWidth();
-				
-				$("#slide-show").css("height",imageHeight + 1);
-				//$("#debug").text("calculating by width")
-			}
-				//$("#debug2").text("width calculated")
-		} else {
-			if (windowHeight > 500) {
-				//imageWidth = ((windowHeight)*(1024/655))-15;
-				imageWidth = ((windowHeight)*(1366/768))-15;
-				$("#wrapper").css("width", imageWidth);	
-				$("#slide-show").css("width", imageWidth );	
-				$("#slide-image").css("width", imageWidth);
-				imageHeight = $("#slide-image>div>img").innerHeight();
-
-				$("#slide-show").css("height",imageHeight + 1);
-				//$("#debug").text("calculating by height")
-			}
-				//$("#debug2").text("height calculated")
-		}
 		
-		initialSlide(imageIndex);
-	}
-
-
-
-	$(document).ready(function () {
-		
-		/*Initialize Image Carrousel Functions*/
-
-		imgCount = $("#slide-image>div").length;
-		initialSlide(imageIndex);
+		mediaRespond();
 		autoSlide();
 		
 		$(".slide-next").click(nextSlide);
 		$(".slide-prev").click(prevSlide);
-
-		if (isIpad) {
-			$("#slide-image").swipe( {
-				swipeLeft:function(event, direction, distance, duration, fingerCount) {
-				nextSlide();	
-				},
-				swipeRight:function(event, direction, distance, duration, fingerCount) {
-				prevSlide();	
-				}
-			});
-		}
-		
+	
 		$("#slide-show").hover(
 			function () {
-			clearInterval(loopTimer);
+				clearInterval(loopTimer);
 			},
 			function () {
-			autoSlide();
+				autoSlide();
 			}
 		);
 
-
-	mediaRespond();
-
-	});									// Closing Document Ready Function
-
-	$(window).load(function () {
-		
-	/*Initiate Responsive Media Functions*/
-
 		$(window).resize(function () {
-			mediaRespond();
+				mediaRespond();
 		});
 
-		mediaRespond();
-	});									// Closing Window Load Function
-})(jQuery)
+	}()
+
+	return app;
+}());
